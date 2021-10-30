78fb5fa5576b070b6ae4c85e3650ccdae8e88dce
Ceri Trammell <ceritrammell@Ceris-Air.lan>
2021-10-30 15:35:30 -0500

    html initial set up

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var currentDate = moment().format(MM/DD/YYYY)
+
+
+var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=8d7a67ef47192bdaaee4e3e539a24175"
