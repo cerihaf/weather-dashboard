c0b4e75c4444b408d09495dbb094f23e476bdab1
Ceri Trammell <ceritrammell@Ceris-Air.lan>
2021-10-31 15:22:28 -0500

    finished functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var humidity = document.getElementById("humidity");
 var uvIndex = document.getElementById("uvIndex");
-var cards = document.getElementById("forecastCards");
+var forecastCards = document.getElementById("forecastCards");
 var searchHistory = document.getElementById("searchHistory");
 
 var savedCities = []
@@ -36,6 +36,7 @@ function getWeather(query) {
       }).catch(function(error){
           console.log(error)
       })
+
 };
 
 function displayWeather(weather, cityName){
@@ -55,6 +56,34 @@ function displayWeather(weather, cityName){
             console.log(Math.floor(weather.current.uvi))
             uvIndex.style.backgroundColor = colors[Math.floor(weather.current.uvi)]
             console.log(colors[weather.current.uvi])
+        }
+    var cards = weather.daily
+        for(var i = 0; i<5; i++){
+            var column = document.createElement("div")
+            column.classList.add("col-md-auto", "col-sm-12")
+            var card= document.createElement("div")
+            card.classList.add("card", "bg-secondary", "text-white", "p-1", "rounded-0")
+            var date = document.createElement("h4")
+                date.textContent = moment(cards[i].dt, "X").format("MM/DD/YYYY");
+            card.append(date)
+            var img = document.createElement("img")
+                img.classList.add("img-fluid", "w-25")
+                img.setAttribute("src", `http://openweathermap.org/img/wn/${cards[i].weather[0].icon}@2x.png`)
+                img.setAttribute("alt", cards[i].weather.description)
+            card.append(img)
+            var list = document.createElement("ul")
+            var item = document.createElement("li")
+                item.innerHTML = `Temp: ${Math.floor((cards[i].temp.max-273.15) * (9/5) +32)} &#8457;`
+                list.append(item)
+                item = document.createElement("li")
+                item.textContent = `Wind: ${cards[i].wind_speed} MPH`
+                list.append(item)
+                item = document.createElement("li")
+                item.textContent = `Humidity: ${cards[i].humidity} %`
+                list.append(item)
+            card.append(list)
+            column.append(card)
+            forecastCards.append(column)
         }
 };
 
